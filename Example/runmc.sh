f5206b1f056e01370fe0f79a53f4b8b5323736cf
andriish <andriish@users.noreply.github.com>
2016-11-20 00:42:17 +0100

    ex

---
+++ Example/runmc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+export GFORTRAN_CONVERT_UNIT='native'
+convert_example_JADE.exe hepmc2_jade  $1   temp.jade Mode=0 > convert.log
+if [ -n temp.jade ]; then
+echo "Conversion on ->"$1"<-has failed. See convert.log"
+exit
+fi
+mv  temp.jade mcjadeinput.cprod
+########################################################################
+export GFORTRAN_CONVERT_UNIT='native'
+cat mcjade.card   | mcjade > mcjade.log
+########################################################################
+export GFORTRAN_CONVERT_UNIT='big_endian;native:2'
+cat superv.card   | superv > superv.log
+########################################################################
+export GFORTRAN_CONVERT_UNIT='native;big_endian:2,22'
+cat ze4v.card     | ze4v > ze4v.log
+########################################################################
+export GFORTRAN_CONVERT_UNIT='native'
+cat  jzread.card  | jzread >jzread.log
+########################################################################
+export GFORTRAN_CONVERT_UNIT='native;big_endian:2,22'
+cat  jtjob.card   | jtjob >jtjob.log
+########################################################################
+mkdir -p output
+mv *.stat *.bos *.ze4v *.root *.histo *.cprod *.log output
+
+
--- Tests/genmc64.sh
+++ Tests/genmc64.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/usr/lib64/SHERPA-MC:$LD_LIBRARY_PATH
-export PATH=$PATH:$(pwd)/../jadesoft/bin:./
+export PATH=$PATH:$(pwd)/../jadesoft/bin:$(pwd)/../HepMC3/outputs/bin/:$(pwd)/../jtuple/bin:$(pwd)/../jtuple
 export BLAS_LIBRARY_PATH=/usr/lib64
 export BLAS_SEARCH_LIBS=/usr/lib64/libblas.so
 #######################Compile HepMC3###################################
@@ -29,8 +29,8 @@ cd  ../Tests
 Sherpa -f Runeesherpa_0.12.dat EVENT_OUTPUT=HepMC_GenEvent[out.hepmc2] HEPMC_TREE_LIKE=1  EVENTS=100
 mv out.hepmc2.hepmc2g   sherpa34gev.hepmc2
 ########################################################################
-../HepMC3/outputs/bin/convert_example_JADE.exe hepmc2_jade  sherpa34gev.hepmc2   bsherpa34gev.jade Mode=0:events_limit=100
-../HepMC3/outputs/bin/convert_example_JADE.exe hepmc2_jade  sherpa34gev.hepmc2   asherpa34gev.jade Mode=1:events_limit=100
+convert_example_JADE.exe hepmc2_jade  sherpa34gev.hepmc2   bsherpa34gev.jade Mode=0:events_limit=100
+convert_example_JADE.exe hepmc2_jade  sherpa34gev.hepmc2   asherpa34gev.jade Mode=1:events_limit=100
 ########################################################################
 export GFORTRAN_CONVERT_UNIT='native'
 cat mcjadecard.txt   | mcjade
