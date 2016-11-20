0b5856a2e826a54fbcc7eb6618e932a1f96350cf
andriish <andriish@users.noreply.github.com>
2016-11-20 19:59:13 +0100

    OK

--- run.sh
+++ run.sh
@@ -3,13 +3,13 @@
 set -x
 export TOP=$(pwd)/installed
 mkdir -p $TOP
-cd HepMC3/
-rm -rf outputs CMakeFiles  CMakeCache.txt
-cmake CMakeLists.txt -DCMAKE_Fortran_COMPILER=gfortran  -DHEPMC_ENABLE_ROOTIO=ON -DCMAKE_INSTALL_PREFIX=$TOP
-make -f Makefile clean
-make -f Makefile -j 8
-make -f Makefile  install
-cd ..
+#cd HepMC3/
+#rm -rf outputs CMakeFiles  CMakeCache.txt
+#cmake CMakeLists.txt -DCMAKE_Fortran_COMPILER=gfortran  -DHEPMC_ENABLE_ROOTIO=ON -DCMAKE_INSTALL_PREFIX=$TOP
+#make -f Makefile clean
+#make -f Makefile -j 8
+#make -f Makefile  install
+#cd ..
 ########################################################################
 cd picocernlib
 rm -rf outputs CMakeFiles  CMakeCache.txt
@@ -18,6 +18,7 @@ make -f Makefile clean
 make -f Makefile -j 8
 make install
 cd ..
+
 ########################################################################
 cd jadesoft
 rm -rf outputs CMakeFiles  CMakeCache.txt
