b80391891aa274a6336ce49c96269fe25ee1614e
andriish <andriish@users.noreply.github.com>
2020-10-06 20:24:11 +0200

    Implement CI (#1)
    * Removed HepMC3
    
    * Updated converter and  ported it to newer HepMC3
    
    * Added dependencies
    
    * Cleanup cmake
    
    Co-authored-by: Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>

---
+++ .github/workflows/centos7/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -l
+set -x
+uname -a 
+cat /etc/issue
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y install  gcc gcc-c++ gcc-gfortran make
+yum -y install  cmake3 cmake3-data
+yum -y install  HepMC3*
+yum -y install  lapack-static  lapack-devel lapack  gengetopt  blas-devel blas atlas-devel atlas
+yum -y install  libX11-devel libX11  libXmu-devel libXmu libXau-devel libXau libXcursor-devel  libXcursor  libSM-devel libSM libICE libICE-devel libXext-devel libXext
+
+yum -y install  root-*6* --exclude=*doc* --exclude=*debug* --skip-broken
+
+export CMAKE=cmake3
+sh run.sh
+
+out=$?
+echo ::set-output name=out::$out
--- macrun.sh
+++ macrun.sh
@@ -1,33 +1,22 @@
 #!/bin/bash
 #######################MAC setup###################################
-#export ROOTSYS=/Applications/root_v6.09.02/
 #. /Applications/root_v6.09.02/bin/thisroot.sh
-export ROOTSYS=/usr/local/
-export PATH=/usr/local/bin:/opt/subversion/bin:$PATH
-export MACOSX_DEPLOYMENT_TARGET=10.12
 #sudo /Applications/CMake.app/Contents/bin/cmake-gui --install=/usr/local/bin
+export PATH=/usr/local/bin:$PATH
+export MACOSX_DEPLOYMENT_TARGET=10.12
 mkdir -p jadesoft/bin
 
 #F_UFMTENDIAN="little;big:10,20" should work with intel
 
-#######################Compile HepMC3###################################
 set -x
 export TOP=$(pwd)/installed
 mkdir -p $TOP
-cd HepMC3/
-rm -rf outputs CMakeFiles  CMakeCache.txt
-#cmake CMakeLists.txt -DCMAKE_Fortran_COMPILER=gfortran  -DHEPMC_ENABLE_ROOTIO=ON -DCMAKE_INSTALL_PREFIX=$TOP
-cmake CMakeLists.txt -DCMAKE_Fortran_COMPILER=gfortran  -DHEPMC_ENABLE_ROOTIO=OFF -DCMAKE_INSTALL_PREFIX=$TOP
-make -f Makefile clean
-make -f Makefile -j 8
-make -f Makefile  install
-cd ..
 ########################################################################
 cd picocernlib
 rm -rf outputs CMakeFiles  CMakeCache.txt
 cmake CMakeLists.txt -DCMAKE_Fortran_COMPILER=gfortran -DCMAKE_INSTALL_PREFIX=$TOP
 make -f Makefile clean
-make -f Makefile -j 8
+make -f Makefile -j 8   ||  { echo 'make failed' ; exit 1; }
 make install
 cd ..
 
@@ -36,15 +25,15 @@ cd jadesoft
 rm -rf outputs CMakeFiles  CMakeCache.txt
 cmake -Wno-dev CMakeLists.txt -DCMAKE_Fortran_COMPILER=gfortran  -DCMAKE_INSTALL_PREFIX=$TOP  -DPICOCERNLIB=$TOP/lib/libpicocernlib.a
 make -f Makefile clean
-make -f Makefile -j 8
+make -f Makefile -j 8  ||  { echo 'make failed' ; exit 1; }
 make install
 cd ..
 ########################################################################
 cd convert
 rm -rf outputs CMakeFiles  CMakeCache.txt
-cmake CMakeLists.txt -DCMAKE_Fortran_COMPILER=gfortran  -DCMAKE_INSTALL_PREFIX=$TOP  -DHEPMC3_ROOT_DIR=$TOP 
+cmake CMakeLists.txt -DCMAKE_Fortran_COMPILER=gfortran  -DCMAKE_INSTALL_PREFIX=$TOP
 make -f Makefile clean
-make -f Makefile -j 8
+make -f Makefile -j 8  ||  { echo 'make failed' ; exit 1; }
 make install
 cd ..
 
@@ -53,7 +42,7 @@ cd jtuple
 rm -rf outputs CMakeFiles  CMakeCache.txt
 cmake CMakeLists.txt -DCMAKE_Fortran_COMPILER=gfortran  -DCMAKE_INSTALL_PREFIX=$TOP  -DPICOCERNLIB=$TOP/lib/libpicocernlib.a -DJADELIB_ROOT_DIR=$TOP
 make -f Makefile clean
-make -f Makefile -j 8
+make -f Makefile -j 8  ||  { echo 'make failed' ; exit 1; }
 make install
 cd ..
 ########################################################################
@@ -61,6 +50,6 @@ cd fptobos
 rm -rf outputs CMakeFiles  CMakeCache.txt
 cmake CMakeLists.txt -DCMAKE_Fortran_COMPILER=gfortran  -DCMAKE_INSTALL_PREFIX=$TOP  -DPICOCERNLIB=$TOP/lib/libpicocernlib.a -DJADELIB_ROOT_DIR=$TOP
 make -f Makefile clean
-make -f Makefile -j 8
+make -f Makefile -j 8  ||  { echo 'make failed' ; exit 1; }
 make install
 cd ..
