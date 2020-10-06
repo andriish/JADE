f2987e55721b5bfc203fdde6e259490a0e87faa6
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2020-10-06 18:09:06 +0200

    Fix cmake

--- .github/workflows/centos8/entrypoint.sh
+++ .github/workflows/centos8/entrypoint.sh
@@ -4,12 +4,12 @@ uname -a
 cat /etc/issue
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 yum -y install  gcc gcc-c++ gcc-gfortran make
-yum -y install  cmake*
+yum -y install  cmake cmake-data
 yum -y install  HepMC3*
 yum -y install  lapack-static  lapack-devel lapack  gengetopt  blas-devel blas
 yum -y install  libX11-devel libX11  libXmu-devel libXmu libXau-devel libXau libXcursor-devel  libXcursor
 
-yum -y install  *root-*6* --exclude=*doc* --exclude=*debug* --skip-broken
+yum -y install  root-*6* --exclude=*doc* --exclude=*debug* --skip-broken
 
 sh run.sh
 
