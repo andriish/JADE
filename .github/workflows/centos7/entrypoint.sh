31f2a5dd2001c3d36a7a5018cfc0e74c1c797f75
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2020-10-06 17:53:57 +0200

    Fix cmake

--- .github/workflows/centos7/entrypoint.sh
+++ .github/workflows/centos7/entrypoint.sh
@@ -4,7 +4,7 @@ uname -a
 cat /etc/issue
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y install  gcc gcc-c++ gcc-gfortran make
-yum -y install  cmake*
+yum -y install  cmake3*
 yum -y install  HepMC3*
 yum -y install  lapack-static  lapack-devel lapack  gengetopt
 yum -y install  libX11-devel libX11  libXmu-devel libXmu libXau-devel libXau libXcursor-devel  libXcursor
--- .github/workflows/osx/entrypoint.sh
+++ .github/workflows/osx/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 set -x
-brew install wget coreutils gfortran
+brew install wget coreutils gcc
 brew tap davidchall/hep
 brew install hepmc3
 wget https://root.cern/download/root_v6.22.02.macosx64-10.15-clang110.tar.gz
