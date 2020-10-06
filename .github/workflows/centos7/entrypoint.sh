6f9ac56d1c268c151f88cb0fed8405a804ed44f5
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2020-10-06 18:42:23 +0200

    Cleanup cmake

--- .github/workflows/centos7/entrypoint.sh
+++ .github/workflows/centos7/entrypoint.sh
@@ -6,7 +6,7 @@ yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarc
 yum -y install  gcc gcc-c++ gcc-gfortran make
 yum -y install  cmake3 cmake3-data
 yum -y install  HepMC3*
-yum -y install  lapack-static  lapack-devel lapack  gengetopt  blas-devel blas
+yum -y install  lapack-static  lapack-devel lapack  gengetopt  blas-devel blas atlas-devel atlas
 yum -y install  libX11-devel libX11  libXmu-devel libXmu libXau-devel libXau libXcursor-devel  libXcursor  libSM-devel libSM libICE libICE-devel libXext-devel libXext
 
 yum -y install  root-*6* --exclude=*doc* --exclude=*debug* --skip-broken
