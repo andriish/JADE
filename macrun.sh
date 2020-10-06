ded5c426c601b2c35bac35e92a9352961e786ed5
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2020-10-06 16:02:58 +0200

    Try CI

--- macrun.sh
+++ macrun.sh
@@ -14,14 +14,6 @@ mkdir -p jadesoft/bin
 set -x
 export TOP=$(pwd)/installed
 mkdir -p $TOP
-cd HepMC3/
-rm -rf outputs CMakeFiles  CMakeCache.txt
-#cmake CMakeLists.txt -DCMAKE_Fortran_COMPILER=gfortran  -DHEPMC_ENABLE_ROOTIO=ON -DCMAKE_INSTALL_PREFIX=$TOP
-cmake CMakeLists.txt -DCMAKE_Fortran_COMPILER=gfortran  -DHEPMC_ENABLE_ROOTIO=OFF -DCMAKE_INSTALL_PREFIX=$TOP
-make -f Makefile clean
-make -f Makefile -j 8
-make -f Makefile  install
-cd ..
 ########################################################################
 cd picocernlib
 rm -rf outputs CMakeFiles  CMakeCache.txt
