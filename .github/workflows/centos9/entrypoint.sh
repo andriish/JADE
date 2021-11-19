e2144889534c8ea59379336e3440f768e95a2a00
Andrii Verbytskyi <averbyts@cern.ch>
2021-11-19 18:00:27 +0100

    PK

--- .github/workflows/centos9/entrypoint.sh
+++ .github/workflows/centos9/entrypoint.sh
@@ -5,7 +5,8 @@ cat /etc/issue
 dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
 dnf -y install dnf-plugins-core
 dnf config-manager --set-enabled powertools
-dnf -y install  gcc gcc-c++ gcc-gfortran make which cmake cmake-data cmake-filesystem HepMC3*
+dnf -y install  gcc gcc-c++ gcc-gfortran make which cmake cmake-data cmake-filesystem 
+dnf -y install  HepMC3*
 dnf -y install  lapack-static  lapack-devel lapack  gengetopt  blas-devel blas atlas-devel atlas  openblas-devel openblas openblas-serial64 openblas-threads  --skip-broken
 dnf -y install  libX11-devel libX11  libXmu-devel libXmu libXau-devel libXau libXcursor-devel  libXcursor  libSM-devel libSM libICE libICE-devel libXext-devel libXext
 dnf -y install  root-*6* --exclude=*doc* --exclude=*debug* --skip-broken
