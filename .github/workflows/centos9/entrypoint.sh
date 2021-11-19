043328e4ef970f0ebca6488d86dff863380085da
Andrii Verbytskyi <averbyts@cern.ch>
2021-11-19 17:56:19 +0100

    PK

--- .github/workflows/centos9/entrypoint.sh
+++ .github/workflows/centos9/entrypoint.sh
@@ -2,16 +2,17 @@
 set -x
 uname -a 
 cat /etc/issue
-yum -y install  epel-release dnf-plugins-core
+dnf -y install epel-release
+dnf -y install dnf-plugins-core
 dnf config-manager --set-enabled powertools
-yum -y install  gcc gcc-c++ gcc-gfortran make which cmake cmake-data cmake-filesystem HepMC3*
-yum -y install  lapack-static  lapack-devel lapack  gengetopt  blas-devel blas atlas-devel atlas  openblas-devel openblas openblas-serial64 openblas-threads  --skip-broken
-yum -y install  libX11-devel libX11  libXmu-devel libXmu libXau-devel libXau libXcursor-devel  libXcursor  libSM-devel libSM libICE libICE-devel libXext-devel libXext
-yum -y install  root-*6* --exclude=*doc* --exclude=*debug* --skip-broken
+dnf -y install  gcc gcc-c++ gcc-gfortran make which cmake cmake-data cmake-filesystem HepMC3*
+dnf -y install  lapack-static  lapack-devel lapack  gengetopt  blas-devel blas atlas-devel atlas  openblas-devel openblas openblas-serial64 openblas-threads  --skip-broken
+dnf -y install  libX11-devel libX11  libXmu-devel libXmu libXau-devel libXau libXcursor-devel  libXcursor  libSM-devel libSM libICE libICE-devel libXext-devel libXext
+dnf -y install  root-*6* --exclude=*doc* --exclude=*debug* --skip-broken
 
 sh jadeinstall.sh
 
-yum -y install pythia8-devel pythia8 pythia8-data 
+dnf -y install pythia8-devel pythia8 pythia8-data 
 
 sh jadetest.sh
 
