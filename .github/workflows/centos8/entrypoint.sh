1c42ee3f001d5f1df6b931c54944e50078447668
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2023-01-25 13:38:51 +0100

    Try a better CI

--- .github/workflows/centos8/entrypoint.sh
+++ .github/workflows/centos8/entrypoint.sh
@@ -2,7 +2,6 @@
 set -x
 uname -a 
 cat /etc/issue
-cd /etc/yum.repos.d/
 sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 yum -y install  epel-release dnf-*
