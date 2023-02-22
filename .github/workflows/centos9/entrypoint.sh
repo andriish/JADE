c6fb535c211f9a6209376786a2cbe475071a2c25
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2023-02-22 18:20:20 +0100

    Use alma

--- .github/workflows/centos9/entrypoint.sh
+++ .github/workflows/centos9/entrypoint.sh
@@ -2,16 +2,16 @@
 set -x
 uname -a 
 cat /etc/issue
-yum -y install  epel-release dnf-plugins-core
+yum -y install  epel-release dnf-plugins-core  --exclude=*uploa* --exclude=*product* --exclude=*subscr*    epel*
 dnf config-manager --set-enabled powertools
 yum -y install  gcc gcc-c++ gcc-gfortran make which cmake cmake-data cmake-filesystem HepMC3*
 yum -y install  lapack-static  lapack-devel lapack  gengetopt  blas-devel blas atlas-devel atlas  openblas-devel openblas openblas-serial64 openblas-threads  --skip-broken
 yum -y install  libX11-devel libX11  libXmu-devel libXmu libXau-devel libXau libXcursor-devel  libXcursor  libSM-devel libSM libICE libICE-devel libXext-devel libXext
 yum -y install  root-*6* --exclude=*doc* --exclude=*debug* --skip-broken
 yum -y install pythia8-devel pythia8 pythia8-data 
-yum -y  install  dnf-plugins-core --exclude=*uploa* --exclude=*product* --exclude=*subscr*
+yum -y install  dnf-plugins-core --exclude=*uploa* --exclude=*product* --exclude=*subscr*
 dnf -y copr enable averbyts/HEPrpms 
-yum -y  install cernlib*
+yum -y install cernlib*
 
 sh jadeinstall.sh
 
